e440f5acc9fcde84014961ecfa9425b1aa9a99c5
KMKR0910 <kasunrashmika5@gmail.com>
2026-09-15 22:35:20 +0530

    feat: implement intent detection

---
+++ app/chatbot/intents.py
@@ -0,0 +1,19 @@
+GREETING = "greeting"
+
+PASSWORD_RESET="password_reset"
+
+LOGIN_PROBLEM ="login_problem"
+
+TICKET_CREATE ="ticket_create"
+
+TICKET_STATUS ="ticket_status"
+
+SYSTEM_ERROR = "system_error"
+
+WORKING_HOURS="working_hours"
+
+HUMAN_SUPPORT ="human_support"
+
+GOODBYE ="goodbye"
+
+UNKNOWN ="unknown"
